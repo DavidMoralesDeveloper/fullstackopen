e25a2e1c043fef75ad72a8310e5a67a2c672e6ec
DavidMoralesDeveloper <david.morales.developer.01@gmail.com>
2022-06-26 12:33:16 -0500

    commit 1.3

--- part1/courseinfo/course-info-exercises/src/index.js
+++ part1/courseinfo/course-info-exercises/src/index.js
@@ -6,43 +6,49 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => <h1>{props.couser}</h1>
 
-const Part = (props) => {
-  return<p> {props.part} {props.exercises} </p>
-}
+  const Part = (props) => {
+    return <p> {props.part} {props.exercises} </p>
+  }
 
-const Content = () => {
-  
-   return(
-    <div>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
-      
-    </div>
-   )
-}
+  const Content = () => {
 
-const Total = (props) => {
-  return <p> Number of exercises {props.exercises} </p>
-}
+    return (
+      <div>
+        <Part part={part1.name} exercises={part1.exercises} />
+        <Part part={part2.name} exercises={part2.exercises} />
+        <Part part={part3.name} exercises={part3.exercises} />
+
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return <p> Number of exercises {props.exercises} </p>
+  }
 
   return (
     <>
       <div>
         <Header couser={course} />
-                
-        <Content  />
-        
-        <Total exercises={exercises1 + exercises2 + exercises3} />
+
+        <Content />
+
+        <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
       </div>
     </>
   )
